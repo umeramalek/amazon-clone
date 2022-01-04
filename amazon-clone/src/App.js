6dba6a19eba8ec6da89a459a5dea8192383befbb
umeramalek <umeramalek796@gmail.com>
2022-01-03 22:40:18 -0800

    started the header component

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1> Hello Clever Programmer, Let's build amazon store 
-      </h1>
+      <h1> Hello World </h1>
+      {/* {Header} */}
+      {/* {Home} */}
     </div>
   );
 }
