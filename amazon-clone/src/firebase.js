c44c996466de88d69505295eca81378f7a8da7ae
umeramalek <umeramalek796@gmail.com>
2022-01-03 22:20:33 -0800

    got rid of files not needed and added a h1 tag to start from clean slate

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyALskDA4wtVys1-lJtqqXP64ET4QZB5oM4",
-    authDomain: "clone-a01aa.firebaseapp.com",
-    projectId: "clone-a01aa",
-    storageBucket: "clone-a01aa.appspot.com",
-    messagingSenderId: "213498363313",
-    appId: "1:213498363313:web:019e8671638e037f951190",
-    measurementId: "G-34K22YEMEG"
-  };
+  apiKey: "AIzaSyALskDA4wtVys1-lJtqqXP64ET4QZB5oM4",
+  authDomain: "clone-a01aa.firebaseapp.com",
+  projectId: "clone-a01aa",
+  storageBucket: "clone-a01aa.appspot.com",
+  messagingSenderId: "213498363313",
+  appId: "1:213498363313:web:019e8671638e037f951190",
+  measurementId: "G-34K22YEMEG"
+};
