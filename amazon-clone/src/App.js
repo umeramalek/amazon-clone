c0d303e10536d28c383f164a1ef21e44438559d2
umeramalek <umeramalek796@gmail.com>
2022-01-03 23:34:49 -0800

    install material ui and added search icon

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -5,7 +5,6 @@ import Header from './Header'
 function App() {
   return (
     <div className="app">
-      <h1> Hello World </h1>
       <Header />
       {/* Home */}
     </div>
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
     return (
         <div className = 'header'>
-            <img className= "header_logo"  src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+            <img className= "header__logo"  src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
 
             <div className="header__search">
-                <input className="header__input" type="text"></input>
+                <input className="header__searchInput" type="text"/>
+                <SearchIcon className="header__searchIcon" />
                 {/* Logo */}
             </div>
 
