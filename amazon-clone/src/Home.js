256722e9c2eba9ee3f27f45adcc95ec08cd7897e
umeramalek <umeramalek796@gmail.com>
2022-01-13 22:57:08 -0800

    added product containers

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -6,6 +6,22 @@ function Home() {
         <div className="home">
             <div className="home__container">
                 <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
+                
+                <div className="home__row">
+                    {/* product */}
+                    {/* product */} 
+                </div>
+
+                <div className="home__row">
+                    {/* product */}
+                    {/* product */} 
+                    {/* product */} 
+                </div>
+
+                <div className="home__row">
+                    {/* product */}
+                </div>
+            
             </div>
         </div>
     )
