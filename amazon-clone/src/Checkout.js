6c71bd10dea2fe2eff148054ba767ba80b3c8050
umeramalek <umeramalek796@gmail.com>
2022-05-15 20:23:27 -0700

    checkout products works

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Checkout.css'
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal from "./Subtotal"
 
 function Checkout() {
+
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout__left">
@@ -10,10 +15,17 @@ function Checkout() {
 
                 <div>
                     <h2 className="checkout__title"> Your shopping Cart</h2>
-                    {/* CheckoutProduct */}
-                    {/* CheckoutProduct */}
-                    {/* CheckoutProduct */}
-                    {/* CheckoutProduct */}
+
+                    {basket.map(item => 
+                        (<CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />))
+                    }
+
                 </div>
 
             
